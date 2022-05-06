5a775115bd666cd5236da705886f56ca9d76bfb6
Julio Barros <juliocezarbarros2@gmail.com>
2022-05-06 09:05:20 -0300

    Atividade de CRUD GEN

---
+++ src/main/java/com/gamehight/play/model/Produto.java
@@ -0,0 +1,78 @@
+package com.gamehight.play.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome; 
+	
+	private String desenvolvedor;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Positive
+    private BigDecimal valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDesenvolvedor() {
+		return desenvolvedor;
+	}
+
+	public void setDesenvolvedor(String desenvolvedor) {
+		this.desenvolvedor = desenvolvedor;
+	}
+
+	
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	} 
+	
+	
+}
